03ae7c537a29d7acb2a9cf4d2fb1e2e347e511e9
muhammadnaeemtahir <m.naeemtahir@hotmail.com>
2024-02-07 16:30:02 +0500

    URLshorten API implemented

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// Add middleware to parse incoming request bodies as JSON
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+// shorturl API endpoint
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+  const urlRegex = new RegExp(
+    "^(http|https)://(www.)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$",
+    "i"
+  );
+  if (!urlRegex.test(url)) {
+    return res.json({ error: "invalid url" });
+  }
+  const shortUrl = Math.floor(Math.random() * 1000);
+  return res.json({ original_url: url, short_url: shortUrl });
+});
+
+//When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const { short_url } = req.params;
+  return res.redirect(short_url);
+});
+
+app.listen(port, function () {
+  console.log(`https://localhost:${port}`);
 });
